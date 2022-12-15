7540dc23079bfc29f572632114e6f958ffd6ec19
jaesungahn91 <jaesungahn91@gmail.com>
2022-12-16 00:21:26 +0900

    til

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,7 @@ module.exports = {
     description: 'Today I Learned',
     base: '/TIL/',
     themeConfig: { //테마별 설정 항목입니다.
+        pageInfo: false,
         sidebar: [
             {
                 title: '2022',
